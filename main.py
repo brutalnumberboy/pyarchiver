be41320f5fc3b4312e848890ea631fd695af9445
brutalnumberboy <scandalzx@protonmail.com>
2025-06-23 23:19:13 +0200

    test

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import crawl_links
 import get_images
+import extract_css
 
 def main():
     crawler = crawl_links.Cralwer()
     urls = crawler.crawl_links()
-    crawler.write_to_file(urls)
-    get_images.get_images()
+    # crawler.write_to_file(urls)
+    # get_images.get_images()
+    crawler.extract_html('https://wallhaven.cc/w/gwjq3d')
+    extract_css.extract_css('https://wallhaven.cc/w/gwjq3d')
 
 
 if __name__ == "__main__":
